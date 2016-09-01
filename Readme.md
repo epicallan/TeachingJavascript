561dbb5b1381facab23fcf53b878ab2b3cbe1adf
allan <epicallan.al@gmail.com>
2016-09-01 18:41:22 +0300

    first lesson

---
+++ Readme.md
@@ -0,0 +1,3 @@
+## For Alex
+
+- Programming lessons
---
+++ helloworld.js
@@ -0,0 +1,23 @@
+// variables store values
+// functions -- execute commands
+// varibales can be constants or subject to change
+
+// a simple run down of the various data structures
+// we have in javascript
+const name = 'Alex'; // its in quotes because its a string
+const age = 13 // its not in quotes coz its a number
+const age2 = 15
+const sum = age + age2
+const list = [3, 3, 4] // a list of numbers
+const listOfnames = ['allan', 'Betty'] // a list of strings
+
+
+// assignment
+// a variable which is constant and set its string to hello world
+
+// function for printing content is console.log
+console.log(' allan is here ');
+console.log(name)
+console.log(' sum of ages: ' + sum)
+// node is a runtime for executing javascript
+// To run script : node helloworld.js to execute your script
